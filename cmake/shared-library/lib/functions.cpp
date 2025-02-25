6558a5b0f80b0cb0d6a4d0e921027672d2e7ddb1
Kiel Cruey <kscruey@gmail.com>
2025-02-24 17:28:46 -0700

    builds but main.exe doesn't get created

---
+++ cmake/shared-library/lib/functions.cpp
@@ -0,0 +1,17 @@
+#include "functions.hpp"
+
+double Functions::add(double num1, double num2) {
+	return num1 + num2;
+}
+
+double Functions::subtract(double num1, double num2) {
+	return num1 - num2;
+}
+
+double Functions::multiply(double num1, double num2) {
+	return num1 * num2;
+}
+
+double Functions::divide(double num1, double num2) {
+	return num1 / num2;
+}
---
+++ cmake/shared-library/shared-library.md
@@ -0,0 +1,9 @@
+# Static Library
+For Windows, the shared library extension will be .dll
+
+## Building
+To build it in debug mode use:
+```
+cmake -S . -B build
+cmake --build build
+```
