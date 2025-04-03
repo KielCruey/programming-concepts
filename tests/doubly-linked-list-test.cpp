24f0de95b088e53f05aa9b8851e5b9a852638c2d
Kiel Cruey <kscruey@gmail.com>
2025-04-02 21:12:54 -0700

    added more doubly linked list tests

--- tests/doubly-linked-list-test.cpp
+++ tests/doubly-linked-list-test.cpp
@@ -51,23 +51,79 @@ TEST_F(DLListTest, IsInList_ElementFound) {
 TEST_F(DLListTest, AddToHead_NullList) {
 	getList()->addToHead(2);
 
-	auto nodeData = getList()->getHead()->data;
-	auto nodeNext = getList()->getHead()->next;
-	auto nodePrevious = getList()->getHead()->previous;
-	auto headLink = getList()->getHead();
-	auto tailLink = getList()->getTail();
-
-	DLLNode<int>* node = new DLLNode<int>(2);
+	auto node1 = getList()->getHead();
 
 	// ===== list tests =====
-	EXPECT_EQ(headLink, tailLink); // links are the same
+	EXPECT_EQ(getList()->getHead(), getList()->getTail()); // links are the same
 
 	// ===== node tests =====
-	EXPECT_EQ(getList()->getHead()->data, nodeData); // node value
-	EXPECT_EQ(nodeNext, nullptr); // node's next link
-	EXPECT_EQ(nodePrevious, nullptr); // node's previous link
+	EXPECT_EQ(node1->data, 2); // node value
+	EXPECT_EQ(node1->next, nullptr); // node's next link
+	EXPECT_EQ(node1->previous, nullptr); // node's previous link
 }
 
 TEST_F(DLListTest, AddToHead_NonNullList) {
+	getList()->addToHead(2);
+	auto node3 = getList()->getHead();
+	getList()->addToHead(3);
+	auto node2 = getList()->getHead();
+	getList()->addToHead(4);
+	auto node1 = getList()->getHead();
 
+	// ===== list tests =====
+	EXPECT_NE(getList()->getHead(), getList()->getTail()); // head/tail aren't the same
+
+	// ===== node tests =====
+	EXPECT_EQ(node1->previous, nullptr); // node1's previous link
+	EXPECT_EQ(node1->next,node2); // node1's next link
+	EXPECT_EQ(node1->data, 4); // node1's data
+
+	EXPECT_EQ(node2->previous, node1); // node2's previous link
+	EXPECT_EQ(node2->next, node3); // node2's next link
+	EXPECT_EQ(node2->data, 3); // node2's data
+
+	EXPECT_EQ(node3->previous, node2);  // node3's previous link
+	EXPECT_EQ(node3->next, nullptr); // node2's next link
+	EXPECT_EQ(node3->data, 2); // node3's data
+}
+
+// ------ addToTail() ------
+TEST_F(DLListTest, AddToTail_NullList) {
+	getList()->addToTail(2);
+	auto node1 = getList()->getTail();
+
+	// ===== list tests =====
+	EXPECT_EQ(getList()->getTail(), getList()->getHead()); // list's head/tail aren't the same
+
+	// ===== node tests =====
+	EXPECT_EQ(node1->previous, nullptr); // node1's previous link
+	EXPECT_EQ(node1->next, nullptr); // node1's next link
+	EXPECT_EQ(node1->data, 2); // node3's data
+}
+
+TEST_F(DLListTest, AddToTail_NonNullList) {
+	getList()->addToTail(2);
+	auto node1 = getList()->getTail();
+	getList()->addToTail(1);
+	auto node2 = getList()->getTail();
+	getList()->addToTail(0);
+	auto node3 = getList()->getTail();
+
+	auto node = new DLLNode<int>(2);
+
+	// ===== list tests =====
+	EXPECT_NE(getList()->getTail(), getList()->getHead());
+
+	// ===== node tests =====
+	EXPECT_EQ(node1->previous, nullptr); // node1's previous link
+	EXPECT_EQ(node1->next, node2); // node1's next link
+	EXPECT_EQ(node1->data, 2); // node1's data
+
+	EXPECT_EQ(node2->previous, node1); // node2's previous link
+	EXPECT_EQ(node2->next, node3); // node2's next link
+	EXPECT_EQ(node2->data, 1); // node2's data
+
+	EXPECT_EQ(node3->previous, node2); // node3's previous link
+	EXPECT_EQ(node3->next, nullptr); // node3's next link
+	EXPECT_EQ(node3->data, 0); // node2's data
 }
