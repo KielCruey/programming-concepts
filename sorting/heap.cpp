39a3c801a2411f9a0db749a4d74447086f21dd0e
Kiel Cruey <kscruey@gmail.com>
2025-11-11 02:18:46 -0700

    heap class into base and derived classes

--- sorting/heap.cpp
+++ sorting/heap.cpp
@@ -1,17 +1,17 @@
-#include "heap.h"
+#include "heap.hpp"
 
-// ======== MinHeap ========
-Heap::Heap(){
-	currentCapacity = NULL;
+// ======== Heap ========
+Heap::Heap() {
 	currentSize = NULL;
-	v = nullptr;
+	currentCapacity = NULL;
+
+	v = new std::vector<int>();
 }
 
-Heap::Heap(std::vector<int>* v)
-	: v(v)
-{
-	currentCapacity = NULL;
-	currentSize = NULL;
+Heap::~Heap() {
+	delete v;
+
+	v = nullptr;
 }
 
 int Heap::parent(int index) {
@@ -26,67 +26,115 @@ int Heap::right(int index){
 	return (2 * index) + 2;
 }
 
-// take in an in vector and makes sure the current values in the tree hold the heap properities
-void Heap::minHeapify(std::vector<int>& V, int index, int newSize) {
+// ======== MaxHeap ========
+MaxHeap::MaxHeap()
+	: Heap()
+{ }
+
+void MaxHeap::heapify(std::vector<int>& v, int index) {
 	int l = left(index);
 	int r = right(index);
-	int min = index; // min heaps index for the smalles value
+	int max = index; // largest value
+
+	// checking left and parent node
+	if (l <= v.size() && v.at(l) > v.at(index)) max = l;
+	else max = index;
+
+	// checking right node and the pervious max
+	if (r <= v.size() && v.at(r) > v.at(max)) max = r;
+
+	if (max != index) {
+		std::swap(v.at(index), v.at(max));
+		heapify(v, max);
+	}
+}
+
+void MaxHeap::buildHeap(std::vector<int>& v, int size){
+}
+
+void MaxHeap::heapSort(std::vector<int>& V, int size) {
+}
+
+void MaxHeap::insert(std::vector<int>& V, int x, int key) {
+}
+
+int MaxHeap::heapMaximum(std::vector<int>* V) {
+	return 0;
+}
+
+int MaxHeap::extractMax(std::vector<int>& V, int newSize) {
+	return 0;
+}
+
+void MaxHeap::increaseKey(std::vector<int>& V, int x, int key) {
+}
+
+// ======== MinHeap ========
+MinHeap::MinHeap() 
+	: Heap()
+{ }
+
+// take in an in vector and makes sure the current values in the tree hold the heap properities
+void MinHeap::heapify(std::vector<int>& v, int index) {
+	int l = left(index);
+	int r = right(index);
+	int min = index; // smallest value
 
 	// l node is within the V size and checking if left node is smaller
-	if (l < newSize && V.at(l) < V.at(index)) min = l; // left index
+	if (l < v.size() && v.at(l) < v.at(index)) min = l; // left index
 	else min = index; // parent
 	
 	// r node is within the V size and checking if right node is smaller
-	if (r < newSize && V.at(r) < V.at(min)) min = r; // right index
+	if (r < v.size() && v.at(r) < v.at(min)) min = r; // right index
 	
 	if (min != index) {
-		std::swap(V.at(index), V.at(min));
-		minHeapify(V, min, newSize);
+		std::swap(v.at(index), v.at(min));
+		heapify(v, min);
 	}
 }
 
 // builds heap with some given vector, while withholding the heap properities
-void Heap::buildMinHeap(std::vector<int>& V, int size) {
+void MinHeap::buildHeap(std::vector<int>& v, int size) {
 	for (int i = size / 2; 0 <= i; i--) {
-		minHeapify(V, i, size);
+		heapify(v, i);
 	}
 }
 
 // takes all of the elements in the vector,
 // and sorts them from lowest element value to the end 
 // and highest element values from the beginning
-void Heap::heapSort(std::vector<int>& V, int size) {
-	buildMinHeap(V, size);
+void MinHeap::heapSort(std::vector<int>& v, int size) {
+	buildHeap(v, size);
 
 	for (int i = size - 1; 1 < size; i--) {
-		std::swap(V[0], V[i]);
+		std::swap(v[0], v[i]);
 		size--;
-		minHeapify(V, 0, size);
+		heapify(v, 0);
 	}
 }
 
 // grabs root node, or returns an exception if vector is empty
-int Heap::heapMinimum(std::vector<int>* V) {
+int MinHeap::heapMinimum(std::vector<int>& v) {
 	// checks if no elements in the heap
-	if (V->size() < 1)
+	if (v.size() < 1)
 		fprintf(stderr, "heap underflow -- empty heap");
 
-	return V->at(0);
+	return v.at(0);
 }
 
 // grabs minimum value, then minHeapifies the heap to hold heap properities
-int Heap::extractMin(std::vector<int>& V, int newSize) {
-	int min = heapMinimum(&V);
-	V.at(0) = V.at(newSize - 1);
+int MinHeap::extractMin(std::vector<int>& v, int newSize) {
+	int min = heapMinimum(v);
+	v.at(0) = v.at(newSize - 1);
 	this->currentSize = newSize - 1;
-	minHeapify(V, 0, this->currentSize);
+	heapify(v, 0);
 
 	return min;
 }
 
 // V is member vector, position is the value in the heap to decrease, key is the new value to replace
 // inserting a value might violate the heap characteristic, while loop makes sure it doesn't
-void Heap::decreaseKey(std::vector<int>& V, int position, int key) {
+void MinHeap::decreaseKey(std::vector<int>& V, int position, int key) {
 	// real index in the vector
 	position--; 
 
@@ -108,19 +156,9 @@ void Heap::decreaseKey(std::vector<int>& V, int position, int key) {
 
 // V is a vector, vSize is the currentSize member variable, and key is the new value to be inserted
 // function adds a value to the end of the vector, then swaps around the values unit it holds the heap properities
-void Heap::insert(std::vector<int>& V, int vSize, int key) {
+void MinHeap::insert(std::vector<int>& V, int vSize, int key) {
 	this->v->push_back(INT_MAX);
-	this->currentCapacity++;
-	this->currentSize++;
 
 	decreaseKey(V, currentSize, key);
 }
 
-// =========== getters/setters ===========
-void Heap::setV(std::vector<int>* v) {
-	this->v = v;
-
-	// side effects of setting v
-	this->currentCapacity = v->capacity();
-	this->currentSize = v->size();
-}
--- sorting/heap.hpp
+++ sorting/heap.hpp
@@ -7,34 +7,66 @@ class Heap
 {
 public:
 	Heap();
-	Heap(std::vector<int>* v);
+	virtual ~Heap();
 
 	// =========== functions ===========
 	int parent(int index);
 	int left(int index);
 	int right(int index);
 
-	void minHeapify(std::vector<int>& V, int index, int newSize);
-	void buildMinHeap(std::vector<int>& V, int size);
-
-	void heapSort(std::vector<int>& V, int size);
-
-	int heapMinimum(std::vector<int>* V);
-	int extractMin(std::vector<int>& V, int newSize);
-
-	void decreaseKey(std::vector<int>& V, int x, int key);
-	void insert(std::vector<int>& V, int x, int key);
+	virtual void heapify(std::vector<int>& V, int index) = 0;
+	virtual void buildHeap(std::vector<int>& V, int size) = 0;
+	virtual void heapSort(std::vector<int>& V, int size) = 0;
+	virtual void insert(std::vector<int>& V, int x, int key) = 0;
 
 	// =========== getters/setters ===========
-	void setV(std::vector<int>* v);
-	std::vector<int>& getV();
+	inline void setV(std::vector<int>* v);
+	inline std::vector<int>* getV() const;
 
 	// =========== variables ===========
-	int currentCapacity;
 	int currentSize;
+	int currentCapacity;
+
 	std::vector<int>* v;
 };
 
-//implement a fibonacci heap to implete the priority queue
+class MaxHeap : public Heap {
+public: 
+	MaxHeap();
+	virtual ~MaxHeap() = default;
+
+	virtual void heapify(std::vector<int>& v, int index) override;
+	virtual void buildHeap(std::vector<int>& v, int size) override;
+	virtual void heapSort(std::vector<int>& v, int size) override;
+	virtual void insert(std::vector<int>& v, int x, int key) override;
+
+	int heapMaximum(std::vector<int>* v);
+	int extractMax(std::vector<int>& v, int newSize);
+	void increaseKey(std::vector<int>& v, int x, int key);
+};
+
+class MinHeap : public Heap {
+public:
+	MinHeap();
+	virtual ~MinHeap() = default;
+
+	virtual void heapify(std::vector<int>& v, int index) override;
+	virtual void buildHeap(std::vector<int>& v, int size) override;
+	virtual void heapSort(std::vector<int>& v, int size) override;
+	virtual void insert(std::vector<int>& v, int x, int key) override;
+
+	int heapMinimum(std::vector<int>& v);
+	int extractMin(std::vector<int>& v, int newSize);
+	void decreaseKey(std::vector<int>& V, int x, int key);
+};
+
+// =========== getters/setters ===========
+void Heap::setV(std::vector<int>* v) {
+	this->v = v;
+}
+
+std::vector<int>* Heap::getV() const {
+	return this->v;
+}
 
 #endif
